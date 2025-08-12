630ea6cccbf7086fd0a1dfa64dba326599164893
nguyenhungict <nguyenduyhung.ict@gmail.com>
2025-08-12 22:51:14 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+require('dotenv').config();
+
+const route = require('./routes/client/index.route');
+
+const app = express()
+const port = process.env.PORT;
+
+app.set('views', './views');
+app.set('view engine', 'pug');
+
+//route
+route(app);
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
